ad65bfe8d483a985aafcd9c80c726a8d44904fd3
Dirk Brockmannn <brockmann.dirk@gmail.com>
2024-01-01 08:12:56 +0100

    1.1.3

--- src/setup_interactions.js
+++ src/setup_interactions.js
@@ -1,10 +1,3 @@
-// here all functions are connected to the widgets, these functions can be defined here, like the 
-// startstop function connected to the go button, or they can be functions defined elsewhere,
-// like the initialization functions, or reset parameter functions. Regardless, here
-// all functions that need to be execuded for example if a controls element is modified, are connected
-// to the update() method of an widget object. See below.
-
-
 import {interval} from "d3"
 import * as ct from "./controls.js"
 import cfg from "./config.js"
@@ -18,19 +11,14 @@ import styles from "./styles.module.css"
 
 var timer = {}
 
-// this defines a startstop simulation function that execute the function iterate() that is defined in simulation.js
-// it also uses information defined in config.js, in this case the delay time between iteration steps.
-
 const startstop = (display,config) => {
 	ct.go.value() == 1 ? timer = interval(()=>iterate(display,config),cfg.simulation.delay) : timer.stop()
 
 }
 
-// this function is called by index.js to connect actions and update functions to the explorables.
-// once this is called, all widgets are usable in the controls panel
-
 const update_cartoon = (controls) => {
-	controls.select("."+styles.attract_scope).attr("d",scope(cfg.widgets.cartoon_size*parameters.attraction_radius.widget.value(),270-parameters.blind_spot.widget.value() / 2))
+	controls.select("."+styles.attract_scope)
+	.attr("d",scope(cfg.widgets.cartoon_size*parameters.attraction_radius.widget.value(),270-parameters.blind_spot.widget.value() / 2))
 	controls.select("."+styles.orient_scope).attr("d",scope(cfg.widgets.cartoon_size*parameters.alignment_radius.widget.value(),270-parameters.blind_spot.widget.value() / 2))
 	controls.select("."+styles.repell_scope).attr("r",cfg.widgets.cartoon_size*parameters.collision_radius.widget.value())
 	controls.select("."+styles.speed).attr("d",scope(cfg.widgets.cartoon_speed_factor*cfg.widgets.cartoon_size*parameters.speed.widget.value(),90+parameters.wiggle.widget.value() ))
@@ -40,10 +28,9 @@ const update_cartoon = (controls) => {
 
 export default (display,controls,config) => {
 	
-	ct.reset.update(()=>resetparameters(controls))	// one button gets the resetparameters() method defined in resetparameters.js
-	ct.go.update(()=>startstop(display,config)) // one button gets the startstop function defined above
-	ct.setup.update(()=>initialize(display,config)) // this once gets the initialize() method defined in simulation.js
-//	param.number_of_particles.widget.update(()=>initialize(display,config)) // here we say that if a specific parameter is changed, in this case the number of particles, we also re_initialize the system (model and visuals)
+	ct.reset.update(()=>resetparameters(controls))	
+	ct.go.update(()=>startstop(display,config)) 
+	ct.setup.update(()=>initialize(display,config)) 
 	each(ct.sliders,sl => {sl.update(()=>update_cartoon(controls) ) })
 	
 	ct.toggles[0].update(()=>(update(display,config)))
--- src/viz.js
+++ src/viz.js
@@ -1,10 +1,3 @@
-// This is the core module for the implementation of the visualization
-// It's analogous to model.js in terms of its relation to other modules,
-// e.g. it reads the parameters and provides initialize, go and update functions
-// to simulation.js where they get bundled with the analogous functions in model.js
-// the observables and variables exported in model.js, e.g. the quantities
-// used for the actual visualizations are also imported to viz.js
-
 import * as d3 from "d3"
 import param from "./parameters.js"
 import {agents} from "./model.js"
@@ -20,9 +13,6 @@ function agent_color(a){
 	return param.orli_switch.widget.value() ? d3.interpolateRainbow(a.theta / 360) : "black"
 }
 
-// the initialization function, this is bundled in simulation.js with the initialization of
-// the model and effectively executed in index.js when the whole explorable is loaded
-// typically here all the elements in the SVG or CANVAS element are set.
 
 const initialize = (display,config) => {
 
@@ -45,10 +35,6 @@ const initialize = (display,config) => {
 
 };
 
-// the go function, this is bundled in simulation.js with the go function of
-// the model, typically this is the iteration function of the model that
-// is run in the explorable. It contains the code that updates the parts of the display
-// panel as a function of the model quantities.
 
 const go = (display,config) => {
 		
@@ -57,10 +43,6 @@ const go = (display,config) => {
 		.style("fill",agent_color)
 	
 }
-
-// the update function is usually not required for running the explorable. Sometimes
-// it makes sense to have it, e.g. to update the visualization, if a parameter is changed,
-// e.g. a radio button is pressed, when the system is not running, e.g. when it is paused.
 
 const update = (display,config) => {
 	display.selectAll("."+styles.agent)
