0d5e1312746fc83cb5e1ffbd0cd25a9951601d15
Dirk Brockmannn <brockmann.dirk@gmail.com>
2024-04-17 17:21:45 +0200

    cosmo commit

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,5 @@
 export default {
   "title": "Flock'n Roll",
-  "subtitle": "Kollekives Schwarverhalten",
+  "subtitle": "Kollekives Schwarmverhalten",
   "description": "Das Explorable illustriert ein einfaches Modell zum Schwarmverhalten bei Tieren, z.b. bei Vögeln, Fischen und Insekten. Ein Schwarm ensteht durch einfache Wechselwirkungen wie Anziehung, Kollissionsvermeidung und dem Ausrichten an anderen Individuen in unmittelbarer Umgebung.",
   }
